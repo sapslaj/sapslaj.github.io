613f2589d4dce96c6dcc1d3353f4ed1093a2b845
sapslaj <saps.laj@gmail.com>
2015-11-07 02:26:18 -0500

    Simplifying

--- js/collapsable.js
+++ js/collapsable.js
@@ -1,20 +1,14 @@
 $(function() {
   var $parent = $('.collapsable');
+  var $child = $('.collapsable-content');
+
   $parent.find('.collapsable-header').click(function(e) {
-    var $child = $('.collapsable-content');
-    if (!$child.data('original-display')) {
-      $child.data('original-display', $child.css('display'));
-    }
     $child.slideToggle('slow');
   });
 
-  $(window).resize(function() {
-    var $content = $('.collapsable-content');
-    var original_display;
-
-    console.log($content.data('original-display'));
-    if (original_display = $content.data('original-display')) {
-      $content.css('display', original_display);
+  $(window).resize(function () {
+    if ($(window).width() >= 1024) {
+      $child.attr('style', '');
     }
   });
 });
