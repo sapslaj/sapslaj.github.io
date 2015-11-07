d9f2939f729395ccc197b1987519d85d2a45db5a
sapslaj <saps.laj@gmail.com>
2015-11-07 01:36:06 -0500

    Fix Collapsable not setting display property correctly

--- js/collapsable.js
+++ js/collapsable.js
@@ -1,6 +1,20 @@
 $(function() {
   var $parent = $('.collapsable');
   $parent.find('.collapsable-header').click(function(e) {
-    $parent.find('.collapsable-content').slideToggle('slow');
+    var $child = $('.collapsable-content');
+    if (!$child.data('original-display')) {
+      $child.data('original-display', $child.css('display'));
+    }
+    $child.slideToggle('slow');
+  });
+
+  $(window).resize(function() {
+    var $content = $('.collapsable-content');
+    var original_display;
+
+    console.log($content.data('original-display'));
+    if (original_display = $content.data('original-display')) {
+      $content.css('display', original_display);
+    }
   });
 });
